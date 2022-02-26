5c80f028845950b3aef5c8108e491a88c5495d7d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-26 13:06:30 +0000

    Added other sorting algorithms

--- SelectionSort.py
+++ SelectionSort.py
@@ -3,7 +3,7 @@ def selection_Sort(elements):
     for i in range(size - 1):
         min_index = i
         for j in range(min_index+1, size):
-            if elements[j] > elements[min_index]:
+            if elements[j] < elements[min_index]:
                 min_index = j
 
         if i != min_index:
@@ -13,12 +13,13 @@ def selection_Sort(elements):
 
 if __name__ == '__main__':
     test = [
+        [234,12,15,8,61,53,23,27],
         [89,78,61,53,23,21,17,12,9,7,6,2,1],
         [],
         [1,5,8,9],
-        [234,12,15,8,61,53,23,27],
         [5]
     ]
 
     for element in test:
-        print(selection_Sort(element))
+        selection_Sort(element)
+        print(element)
