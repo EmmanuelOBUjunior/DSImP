9acdea45d005ecdca07be46e4bd19bebe9dda2b5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-02-27 18:09:46 +0000

    Merge sort done

--- mergeSort.py
+++ mergeSort.py
@@ -1,22 +1,47 @@
-def mergeTwoSortedList(a, b):
-    sortedList = []
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    mergeTwoSortedList(left, right, arr)
+
+
+
+
+def mergeTwoSortedList(a, b, arr):
     len_a = len(a)
     len_b = len(b)
-    i = j = 0
+    i = j = k = 0
 
     while i < len_a and j < len_b:
         if a[i] < b[j]:
-            sortedList.append(a[i])
+            arr[k] = a[i]
+            k += 1
             i += 1
         else:
-            sortedList.append(b[j])
+            arr[k] = b[j]
             j += 1
+            k += 1
 
-    return sortedList
+    while i < len_a:
+        arr[k] = a[i]
+        i += 1
+    while j < len_b:
+        arr[k] = b[j]
+        j += 1
+        k += 1
 
 
 if __name__ == '__main__':
-    a = [5,8,12,56]
-    b = [7,9,45,51]
-
-    print(mergeTwoSortedList(a,b))
+    # a = [5,8,12,56]
+    # b = [7,9,45,51]
+    arr  = [10,3,15,7,8,23,98,29]
+    # print(mergeTwoSortedList(a,b))
+    merge_sort(arr)
+    print(arr)
--- shellSort.py
+++ shellSort.py
@@ -16,6 +16,6 @@ def shellSort(arr):
 
 
 if __name__ == '__main__':
-    elements = [21,38,29,17,4,25,11,32,9]
+    elements = [21,38,29,17,4,24,11,32,9]
     shellSort(elements)
     print(elements)
